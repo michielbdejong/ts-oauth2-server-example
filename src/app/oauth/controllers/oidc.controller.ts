4da210f0fc6352dffe2d13a5406963084630512f
Michiel de Jong <michiel@pondersource.com>
2024-05-30 12:45:36 +0200

    log user in based on verified email address from OIDC

--- src/app/oauth/controllers/oidc.controller.ts
+++ src/app/oauth/controllers/oidc.controller.ts
@@ -4,13 +4,21 @@ import {
   Render,
   Req,
   Res,
+  HttpStatus,
+  UnauthorizedException,
 } from "@nestjs/common";
 import type { Response, Request } from "express";
 import { PrismaService } from "../../prisma/prisma.service.js";
 import { Issuer, generators } from 'openid-client';
+import { MyCustomJwtService } from "../services/custom_jwt_service.js";
+
+import { User } from "@prisma/client";
+import { DateDuration } from "@jmondi/date-duration";
+
 @Controller("oidc")
 export class OidcController {
   constructor(
+    private readonly jwt: MyCustomJwtService,
     private readonly prisma: PrismaService,
   ) {}
 
@@ -36,5 +44,51 @@ export class OidcController {
     const tokenSet = await client.callback('https://sram-auth-poc.pondersource.net/login-callback.html', params, { code_verifier });
     console.log('received and validated tokens %j', tokenSet);
     console.log('validated ID Token claims %j', tokenSet.claims());
+    const userinfo = await client.userinfo(tokenSet.access_token);
+    console.log('userinfo %j', userinfo);
+    const email = tokenSet.claims().email;
+    if (tokenSet.claims().email_verified === true) {
+      let user: User;
+
+      try {
+        user = await this.prisma.user.findFirstOrThrow({
+          where: {
+            email: {
+              equals: email,
+              mode: "insensitive",
+            },
+          },
+        });
+      } catch (e) {
+        throw new UnauthorizedException(null, { cause: e });
+      }
+
+      user = await this.prisma.user.update({
+        where: { id: user.id },
+        data: {
+          lastLoginAt: new Date(),
+          lastLoginIP: req.ip,
+        },
+      });
+
+      const expiresAt = new DateDuration("30d");
+
+      const token = await this.jwt.sign({
+        userId: user.id,
+        email: user.email,
+
+        iat: Math.floor(Date.now() / 1000),
+        exp: expiresAt.endTimeSeconds,
+      });
+
+      res.cookie("jid", token, {
+        secure: true,
+        httpOnly: true,
+        sameSite: "strict",
+        expires: expiresAt.endDate,
+      });
+
+      res.status(HttpStatus.FOUND).redirect(`/`);
+    }
   }
 }
